2fca904ebf84d425efcae1e30f9fc5d3f3b40aed
Danial Halim <danialhalim680@gmail.com>
2024-09-24 10:47:22 +0800

    Initial Commit

---
+++ Test AI/Test_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_AIApp.swift
+//  Test AI
+//
+//  Created by Danial Halim on 24/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Test_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Test AITests/Test_AITests.swift
@@ -0,0 +1,16 @@
+//
+//  Test_AITests.swift
+//  Test AITests
+//
+//  Created by Danial Halim on 24/09/2024.
+//
+
+import Testing
+
+struct Test_AITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
